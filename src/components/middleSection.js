eaede4729ca7ac3ddb5dd2e1684f4588a0a34643
UnderlordToshi <underlordtoshi@gmail.com>
2020-11-25 12:22:21 -0600

    random commit

--- src/components/middleSection.js
+++ src/components/middleSection.js
@@ -5,7 +5,6 @@ function MiddleSection() {
   return (
     <div id="body">
       Just some dirty boys from the backwoods of Arkansas trying to stay out of trouble.
-
       Links to music will be up soon(hopefully)
     </div>
   );
