c7ae71a9bd101717dab1c593ed1e37b0777fd31f
UnderlordToshi <underlordtoshi@gmail.com>
2020-12-04 19:18:19 -0600

    jpg test

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ function Header() {
   return (
     <div className="App">
       <header className="App-header">
-        <a href="https://www.google.com"><img src="../../public/image/picture/20201204_052720_0000[4].png" alt="band art.png" /></a>
+        <a href="https://www.google.com"><img src="../../public/image/picture/20201204_052720_0000[4].jpg" alt="band art.jpg" /></a>
         <h1>Not on my Nessy!</h1>
       </header>
     </div>
