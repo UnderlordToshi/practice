ca7f93e5a2ec7c94048b5091bbcddddf6beeaef9
UnderlordToshi <underlordtoshi@gmail.com>
2020-12-04 18:48:45 -0600

    fix

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ function Header() {
   return (
     <div className="App">
       <header className="App-header">
-        <a href="https://www.google.com"><img src=NomN.png"../../public/image/NomN.png" alt="band art" /></a>
+        <a href="https://www.google.com"><img src="../../public/image/jpeg.jpg" alt="band art" /></a>
         <h1>Not on my Nessy!</h1>
       </header>
     </div>
