626681d3edf0a7164a469a5646b24bddce2d29e7
UnderlordToshi <underlordtoshi@gmail.com>
2020-12-04 20:35:06 -0600

    ee

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ function Header() {
   return (
     <div className="App">
       <header className="App-header">
-        <a href="https://www.google.com"><img src=NomN.png"../../public/image/picture/NomN.png" alt="Not Nessy" width="500" height="333"/</a>
+        <a href="https://www.google.com"><img src="NomN.png" alt="Not Nessy" width="500" height="333"/</a>
 
       <h1>Not on my Nessy!</h1>
       </header>
