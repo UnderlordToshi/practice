0f5e7a13c833572df0df7447a0b30aa677669e10
UnderlordToshi <underlordtoshi@gmail.com>
2020-12-06 07:35:09 -0600

    fixed header

--- src/components/header.js
+++ src/components/header.js
@@ -4,10 +4,12 @@ import '../styles/header.css';
 function Header() {
   return (
     <div className="App">
-      <header className="App-header">
-        <a href="https://www.google.com"><img src="../../public/image/picture/" alt="band art"</a>
-          <h1>Not on my Nessy!</h1>
-      </header>
+      <html>
+        <header className="App-header">
+          <img src="url('NomN.jpg')" alt="band art.jpg"/>
+          <h2>Not on my Nessy!</h2>
+        </header>
+      </html>
     </div>
   );
 }
