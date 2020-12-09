e510e12701570e4d7b4551f9f7908cb0288c1dc8
UnderlordToshi <underlordtoshi@gmail.com>
2020-12-09 16:36:43 -0600

    random

--- src/components/header.js
+++ src/components/header.js
@@ -4,12 +4,9 @@ import '../styles/header.css';
 function Header() {
   return (
     <div className="App">
-      <html>
-        <header className="App-header">
-
-          <h2>Not on my Nessy!</h2>
-        </header>
-      </html>
+      <header className="App-header">
+        <h2>Not on my Nessy!</h2>
+      </header>
     </div>
   );
 }
