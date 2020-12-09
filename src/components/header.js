1e47a4e321175a39f15db3060867e83008e2cdb2
UnderlordToshi <underlordtoshi@gmail.com>
2020-12-08 23:55:30 -0600

    random

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,7 @@ function Header() {
     <div className="App">
       <html>
         <header className="App-header">
-          <img src="url('NomN.jpg')" alt="band art.jpg"/>
+
           <h2>Not on my Nessy!</h2>
         </header>
       </html>
